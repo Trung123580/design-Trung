1a488aed1d8fa74fb429e3cc225b7f4a27e63f7b
Trung <trungvuong3012@gmail.com>
2023-04-24 23:37:36 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// const a = document.querySelectorAll("a");
+// const item = document.querySelector(".nav-item");
+
+// a.forEach(() => {
+//   this.onclick.classList.add("acvite");
+// });
+// // a.onclick = function () {
+// //   this.style.display = "none";
+// // };
